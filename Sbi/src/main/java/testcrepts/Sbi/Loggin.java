9a97c5105bbd71ef90eba615a33bf4452983bf03
DivyaSwamy <DivyaSwamy@LAPTOP-K95838K8>
2019-02-27 11:36:16 +0530

    My 2nd commit

---
+++ Sbi/src/main/java/testcrepts/Sbi/Loggin.java
@@ -0,0 +1,13 @@
+package testcrepts.Sbi;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Loggin {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Windows.old\\WINDOWS\\WinSxS\\x86_netfx4-browser_files_b03f5f7f11d50a3a_4.0.15522.0_none_ff3b7f942aa41043\\chrome.browser");
+		ChromeDriver driver = new ChromeDriver ();
+		
+		
+	}
+
+}
